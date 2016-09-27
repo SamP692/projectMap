09642bcdfb0ad571adb23589f50a8270bf019adf
Sam Peterson <samuel.e.peterson@gmail.com>
2016-09-27 00:27:50 -0400

    will add working rendering to individual project

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -13,28 +13,51 @@ class Project extends Component {
       name: '',
       components: [],
     };
+    this.createNewComponent = this.createNewComponent.bind(this);
   }
   componentDidMount() {
-    console.log(this.props.params.project);
+    this.getAllComponentsForProject();
   }
   getAllComponentsForProject() {
     const userId = firebase.auth().currentUser.uid;
     const projectId = this.props.params.project;
-    const url = `https://projectmap-bf209.firebaseio.com/users/${userId}/projects/${projectId}.json`;
+    const url = `https://projectmap-bf209.firebaseio.com/users/${userId}/projects/${projectId}/components.json`;
     request.get(url).then((data) => {
       const componentData = data.body;
       let components = [];
       if (componentData) {
-        components = Object.keys(componentData).map((id, idx) => { // NEED TO FINISH
-          return componentData[idx].name; // NEED TO FINISH
+        components = Object.keys(componentData).map((componentId) => {
+          return componentData[componentId].name;
         });
       }
       this.setState({ components });
     });
   }
+  createNewComponent() {
+    const componentName = prompt('Please enter a name for your component');
+    const userId = firebase.auth().currentUser.uid;
+    const projectId = this.props.params.project;
+    const url = `https://projectmap-bf209.firebaseio.com/users/${userId}/projects/${projectId}/components.json`;
+    request.post(url).send({ name: componentName }).catch((err) => {
+      console.log(err);
+    });
+    this.getAllComponentsForProject();
+  }
+  createComponentViews() {
+    let componentKey = 1;
+    return this.state.components.map((component) => {
+      componentKey += 1;
+      return (
+        <div key={componentKey} className="componentView">{component}</div>
+      );
+    });
+  }
   render() {
     return (
-      <h1>Stuff</h1>
+      <div id="componentHouse">
+        <button id="createComponentButton" onClick={this.createNewComponent}>New Component</button>
+        {this.createComponentViews()}
+      </div>
     );
   }
 }
