66dc7194b5e8fca8f240455892960b3b7b40d6ee
Sam Peterson <samuel.e.peterson@gmail.com>
2017-01-28 16:40:49 -0500

    reorganize styling for homepage

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to Project Map</h1>
-    </div>
-  );
-};
-
-export default Home;
+export default class Home extends Component {
+  render() {
+    return (
+      <div >
+        <h1>Welcome to React Mapper</h1>
+        <section id="about">
+          This is a resource to assist in mapping out complext React JS-based applications.
+        </section>
+        <section id="sample">
+          Check out a sample map below and discover the architecture of this React
+          JS-based application!
+        </section>
+      </div>
+    );
+  }
+}
